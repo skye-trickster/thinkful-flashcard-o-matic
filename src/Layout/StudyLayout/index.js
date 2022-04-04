2a130fd1f65e684bd7f479d41e20d2d80eaeb0f0
Vincent Mills <vincentmills273@gmail.com>
2022-04-04 00:15:46 -0400

    Added shuffle functionality to study session

--- src/Layout/StudyLayout/index.js
+++ src/Layout/StudyLayout/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import ContentLayer from "../Common/Content"
 import Heading from "../Common/Heading"
 import NotEnoughCards from "./NotEnoughCards"
@@ -7,11 +7,15 @@ import StudyCard from "./StudyCard"
 function StudyLayout({deck, addCardLink="/", endStudyFunction}) {
     const {cards} = deck
     const [index, setIndex] = useState(0)
+    const [shuffle, setShuffle] = useState(false)
+    const [reference, setReference] = useState([])
 
-    const getCard = () => cards[index]
+    const getCard = () => cards[reference[index]]
     const validCardLength = () => cards && cards.length > 2
-    const restart = () => setIndex(0)
-
+    const restart = () => {
+        shuffleCards()
+        setIndex(0)
+    }
     const nextCard = () => {
         if (index >= cards.length - 1) return false
 
@@ -19,8 +23,39 @@ function StudyLayout({deck, addCardLink="/", endStudyFunction}) {
         return true        
     }
 
+    const orderedIndex = () => cards.map((ignored, index) => index)
+
+    const shuffleIndex = () => {
+        const numbers = cards.map((ignored, index) => index)
+        
+        const randomize = () => Math.floor(Math.random() * numbers.length)
+
+        for (let i = numbers.length - 1, j = randomize(); i > j; --i, j = randomize()) {
+            let temp = numbers[i]
+            numbers[i] = numbers[j]
+            numbers[j] = temp
+        }
+
+        return numbers
+    }
+
+    const shuffleCards = () => {
+        if (! (cards && Object.keys(cards).length)) return
+        setReference( shuffle ? shuffleIndex : orderedIndex() )
+    }
+
+    const startShuffle = () => {
+        if (!window.confirm("Are you sure you want to shuffle the deck?\n\nThis will restart the study session.")) return 
+
+        shuffleCards()
+        restart()
+    }
+
+    useEffect(shuffleCards, [shuffle, cards]) 
+
     if (Object.keys(deck).length === 0) return "Loading Deck..."
 
+    
     return (
         <ContentLayer>
             <Heading title={`Study: ${deck.name}`} />
